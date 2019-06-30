a30348bfaec92066a956c3795eaa0aad5989a3d2
imhuwq <imhuwq@gmail.com>
2019-06-30 02:07:36 -0700

    fix(18.04): fix shadowsocks config files

--- 18.04/003-shadowsocks.sh
+++ 18.04/003-shadowsocks.sh
@@ -4,14 +4,14 @@ source utils.sh
 call_seperator
 echo -e "${Red}Step 1${NC}: install privoxy"
 sudo apt install -y privoxy
-update_file /etc/privoxy/config 'forward-socks5 / 10.100.100.3:1080 .' sudo
+update_file /etc/privoxy/config 'forward-socks5 / 10.10.1.3:1080 .' sudo
 sudo service privoxy restart
 
 call_seperator
 echo -e "${Red}Step 2${NC}: install proxychains"
 sudo apt install -y proxychains
 sudo sed -i -E "s/socks4\s+127.0.0.1\s+9050/# socks4 127.0.0.1 9050/" /etc/proxychains.conf
-update_file /etc/proxychains.conf 'socks5 10.100.100.3 1080' sudo
+update_file /etc/proxychains.conf 'socks5 10.10.1.3 1080' sudo
 
 call_seperator
 echo -e "${Red}Step 3${NC}: setup command alias"
--- 18.04/006-java12.sh
+++ 18.04/006-java12.sh
@@ -8,7 +8,7 @@ echo -e "${Red}Step 1${NC}: add ppa"
 sudo add-apt-repository ppa:linuxuprising/java
 
 call_seperator
-echo -e "${Red}Step 2${NC}: install java11"
+echo -e "${Red}Step 2${NC}: install java12"
 sudo apt update
 sudo apt install -y oracle-java12-installer
 
